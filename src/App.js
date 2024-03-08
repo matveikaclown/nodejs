c450a47ff9e4f0b5c3e2da037e628009e6780d55
Matthew <privalka2003@mail.ru>
2024-03-08 20:33:08 +0400

    title commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul class="navigation">
+      <li><a href="atlas">Atlas</a></li>
+      <li><a href="collection">Collection</a></li>
+      <li><a href="articles">Articles</a></li>
+      <li><a href="news">News</a></li>
+      <li><a href="contacts">Contacts</a></li>
+    </ul>
   );
 }
 
